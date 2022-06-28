10f4562871991d9d3be7d9e0d0168ab039cad0e4
Mohamad Al <mohamada0112@gmail.com>
2022-06-28 14:51:22 +0700

    Add files via upload

--- Database10/index.php
+++ Database10/index.php
@@ -27,6 +27,7 @@ if( !$result){
 </head>
 <body>
 <h1>Daftar Mahasiswa</h1>
+<a href="tambah.php">Tambah data mahasiswa</a>
 <table border="1" cellpadding="10" cellspacing="0">
     <tr>
         <th>No.</th>
---
+++ Database10/tambah.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah data mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah data mahasiswa</h1>
+    <from action="" method="post">
+        <li>
+            <label for="nrp">NRP : </label>
+            <input type="text" name="nrp">
+        </li>
+        <li>
+            <label for="nama">Nama : </label>
+            <input type="text" name="nama">
+        </li>
+        <li>
+            <label for="email">E-mail : </label>
+            <input type="text" name="email">
+        </li>
+        <li>
+            <label for="jurusan">Jurusan : </label>
+            <input type="text" name="jurusan">
+        </li>
+        <li>
+            <button type="submit" name="submit">Tambah Data </button>
+        </li>
+    </from>
+</body>
+</html>
